4e29890c68bac6dc3a055369f226aa6fc78b722f
JeaV2 <102710@glr.nl>
2026-04-07 15:01:49 +0200

    feat: added filter based on name or setnumber

--- hej-lego-world/src/App.jsx
+++ hej-lego-world/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import HeroHeader from "./components/HeroHeader"
 import Sidebar from "./components/Sidebar"
 import SetOverzicht from "./components/SetOverzicht"
@@ -5,13 +7,17 @@ import sets from"./assets/data/sets.json"
 
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const filteredSets = sets.sets.filter(set => {
+    return set.name.toLowerCase().includes(searchTerm) || set.setNumber.includes(searchTerm);
+  });
 
   return (
     <div className="app-shell">
       <HeroHeader />
       <main className="layout">
-        <Sidebar />
-        <SetOverzicht sets={sets.sets} />
+        <Sidebar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <SetOverzicht sets={filteredSets} />
       </main>
     </div>
   )
--- hej-lego-world/src/components/Sidebar.jsx
+++ hej-lego-world/src/components/Sidebar.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react';
 import "./Sidebar.css"
 
-function Sidebar() {
-    const [searchTerm, setSearchTerm] = useState("");
-    
+function Sidebar({ searchTerm, setSearchTerm }) {
+
     return (
         <aside className="sidebar">
             <section className="panel panel--yellow">
@@ -12,7 +10,7 @@ function Sidebar() {
                 </div>
                 <h2>Zoeken</h2>
                 <label className="label" htmlFor="searchInput">Zoek op naam of setnummer</label>
-                <input id="searchInput" className="search-input" type="text" placeholder="Bijv. castle, ghost, 10332..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value.toLowerCase())} />
+                <input id="searchInput" className="search-input" type="text" placeholder="Bijv. castle, ghost, 10332..." onChange={(e) => setSearchTerm(e.target.value.toLowerCase())} />
                 <p className="helper-text">
                     Typ in het veld en filter direct door de beschikbare sets.
                 </p>
